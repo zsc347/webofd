b04c1058723d55c53f148a108fca655a1995c776
zsc347 <zsc347@163.com>
2022-02-16 15:36:56 +0800

    using css font api

--- src/display/FontLoader.ts
+++ src/display/FontLoader.ts
@@ -1,26 +1,28 @@
 import { OFDFontElement } from "../core/schema/OFDFont";
 
 export class FontLoader {
-    private _styleElement;
+    private _docFonts: FontFace[];
 
     constructor() {
-        this._styleElement = document.createElement("style");
-        this._styleElement.id = `OFDJS_FONT_${Date.now()}`;
-        document.documentElement
-            .getElementsByTagName("head")[0]
-            .appendChild(this._styleElement);
+        this._docFonts = [];
     }
 
     public async importFont(cfont: OFDFontElement) {
         const url = await cfont.load();
         if (url) {
             const font = new FontFace(cfont.familyName, `url(${url})`);
-            await font.load();
-            document.fonts.add(font);
+            try {
+                await font.load();
+                document.fonts.add(font);
+                this._docFonts.push(font);
+            } catch (err: unknown) {
+                // ignore
+                console.error(`Failed to load font '${cfont.familyName}'`);
+            }
         }
     }
 
     public dispose() {
-        document.removeChild(this._styleElement);
+        this._docFonts.forEach(font => document.fonts.delete(font));
     }
 }
